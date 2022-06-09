9d9392960915f49f2d4fb15f9ea36e62fdff0981
VadimSvistunov <vadim27112001@gmail.com>
2022-06-09 16:50:04 +0300

    bug[migration error] fix migration files

--- db/migrate/20220609123743_create_orders.rb
+++ db/migrate/20220609123743_create_orders.rb
@@ -1,15 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.references :cars, car_id: true
-      t.references :customers, customer_id: true
       t.datetime :started_at
       t.datetime :finished_at
       t.timestamps
     end
 
-    add_foreign_key :orders, :cars
-    add_foreign_key :orders, :customers
+    add_reference :orders, :customer, index: false
+    add_reference :orders, :car, index: false
 
   end
 end
