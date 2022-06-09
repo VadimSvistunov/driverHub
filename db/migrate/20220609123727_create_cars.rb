824aba5109932bc9b5692ba3d3eaba54bc2ca203
VadimSvistunov <vadim27112001@gmail.com>
2022-06-09 16:00:42 +0300

    migrate fatal

--- db/migrate/20220609123727_create_cars.rb
+++ db/migrate/20220609123727_create_cars.rb
@@ -1,7 +1,9 @@
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
-
+      t.string :model
+      t.integer :price
+      t.boolean :status
       t.timestamps
     end
   end
