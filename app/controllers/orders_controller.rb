b2c8c6ff7e3cace1505d44d37eda8a58ebbd1c89
VadimSvistunov <vadim27112001@gmail.com>
2022-06-15 15:38:57 +0300

    #2 minor fixes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,12 @@ class OrdersController < ApplicationController
 
     def new
         @order = Order.new
+        @car_id = params.permit(:car_id)
     end
 
     def create
         @order = Order.new(order_params)
+        @order.car_id = @car_id
 
         if @order.save
             redirect_to "/"
@@ -25,6 +27,6 @@ class OrdersController < ApplicationController
 
     private
     def order_params
-        params.permit(:car_id, :customer_id, :started_at, :finished_at)
+        params.permit(:customer_id, :started_at, :finished_at)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
     root "cars#index"
 
-    resources :orders, only: [:create, :edit,:update, :new] do
-        resources :cars, only: [:new, :index, :create]
+    resources :cars, only: [:new, :index, :create] do
+        resources :orders, only: [:create, :edit,:update, :new]
     end
 
 
