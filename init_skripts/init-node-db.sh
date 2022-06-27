4e12100c9da50558fdfa4e16cbe3ce78e5ab3660
VadimSvistunov <vadim27112001@gmail.com>
2022-06-27 14:29:30 +0300

    refactor: rabase scripts

--- init_skripts/init-node-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER nodedb;
-	CREATE DATABASE nodedb;
-	GRANT ALL PRIVILEGES ON DATABASE nodedb TO nodedb;
-EOSQL
--- init_skripts/init-rails-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER railsdb;
-	CREATE DATABASE railsdb;
-	GRANT ALL PRIVILEGES ON DATABASE railsdb TO railsdb;
-EOSQL
